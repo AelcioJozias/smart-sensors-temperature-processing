6549163752e054d34f5913a8f0b006b7e19896c5
Jozias <aelciojoziasp@gmail.com>
2025-11-22 18:20:51 -0300

    New project files

---
+++ src/test/java/com/jozias/smartsensors/temperature/processing/TemperatureProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jozias.smartsensors.temperature.processing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemperatureProcessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
